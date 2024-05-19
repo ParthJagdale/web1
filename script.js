532fa838ecab167ba5f919a12913d3b38030c29f
parth <parthjagdale716@gmail.com>
2024-05-19 19:36:29 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const learnMoreBtn = document.getElementById('learn-more-btn');
+
+    learnMoreBtn.addEventListener('click', function() {
+        alert('Thank you for your interest! More information coming soon.');
+    });
+});
